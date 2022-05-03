72669127b739f2d02ce2721beb9a2928d1d82143
vitormsiqueira <vmsvitor@outlook.com>
2022-05-03 17:04:54 -0400

    Add firebase

--- src/App.js
+++ src/App.js
@@ -8,7 +8,24 @@ import GlobalStyle from "./styles/global";
 import Header from "./components/Header";
 import Board from './components/Board';
 
+import database from './services/firebase';
+import { ref, child, get} from "firebase/database";
+
 function App() {
+
+  const dbRef = ref(database);
+  const cardId = 2;
+
+  get(child(dbRef, `tasks/cards/${cardId}`)).then((snapshot) => {
+    if (snapshot.exists()) {
+      console.log(snapshot.val());
+    } else {
+      console.log("No data available");
+    }
+  }).catch((error) => {
+    console.error(error);
+  });
+
   return (
     <DndProvider backend={HTML5Backend}>
       <Header/>
---
+++ src/services/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from 'firebase/database';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export default database;
